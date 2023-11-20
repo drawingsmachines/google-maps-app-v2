b3df04c3168574f9078043874285ee8096fc8d0c
drawingsmachines <contacto@drawingsmachines.com>
2023-11-20 01:08:48 -0300

    borra apikey

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,6 +1,6 @@
 import { Loader } from "@googlemaps/js-api-loader"
 
-export const REACT_APP_GOOGLE_MAPS_KEY = "AIzaSyC1JB_O4DN3v9_BoT1-O4tsdDCrdy8EnAU"
+export const REACT_APP_GOOGLE_MAPS_KEY = ""
 
 /*
 export const loader = new Loader({
